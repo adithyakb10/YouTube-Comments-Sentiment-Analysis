b21fe1644bc1a56d9f3fec428f32a94ae2f5e2fe
Adithya K B <adithyakb93@gmail.com>
2024-08-04 14:44:28 +0530

    fix: Write code for selenium to scroll down for the comments to load

--- app.py
+++ app.py
@@ -33,17 +33,18 @@ stop_words = stopwords.words('english')
 
 def returnytcomments(url):
     data=[]
+    driver = Chrome()
+    wait = WebDriverWait(driver,15)
+    driver.get(url)
+    wait.until(EC.visibility_of_element_located((By.TAG_NAME, "body"))).send_keys(Keys.PAGE_DOWN)   
 
-    with Chrome(executable_path=r'C:\Program Files\chromedriver.exe') as driver:
-        wait = WebDriverWait(driver,15)
-        driver.get(url)
+    for item in range(5): 
+        wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#content")))
+        wait.until(EC.visibility_of_element_located((By.TAG_NAME, "body"))).send_keys(Keys.END)
+        time.sleep(2)
 
-        for item in range(5): 
-            wait.until(EC.visibility_of_element_located((By.TAG_NAME, "body"))).send_keys(Keys.END)
-            time.sleep(2)
-
-        for comment in wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "#content"))):
-            data.append(comment.text)
+    for comment in wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "#content"))):
+        data.append(comment.text)
 
     return data
 
